322219f03d752159e5390ac6018b123da65257e4
OHMphatcha <phatcharaphol@hotmail.com>
2022-01-21 23:43:29 +0700

    transition

--- my-app-component/src/components/Person.vue
+++ my-app-component/src/components/Person.vue
@@ -1,68 +1,77 @@
 <template>
   <li>
-      <h1>{{name}}</h1>
-      <button @click="showDescription(id)">Description</button>
-      <button @click="deleteEmployee(id)">delete</button>
+    <h1>{{ name }}</h1>
+    <button @click="showDescription(id)">Description</button>
+    <button @click="deleteEmployee(id)">delete</button>
+    <transition name="fade">
       <div v-show="isVisible">
-          <p> salary :{{salary}}Bath  department:{{department}}</p>
+        <p>salary :{{ salary }}Bath department:{{ department }}</p>
       </div>
+    </transition>
   </li>
 </template>
 
 <script>
 export default {
-    name: "Person",
-    methods: {
-       showDescription(id){
-           this.$emit("show",id)
-       },
-       deleteEmployee(id){
-           this.$emit("delete",id)
-       }
+  name: "Person",
+  methods: {
+    showDescription(id) {
+      this.$emit("show", id);
     },
-    props:{
-        name:{
-            type:String,
-            required:true,
-        },
-        salary:{
-            type:Number,
-            default:15000
-        },
-        department:{
-            type:String,
-            required:true
-        },
-        id:{
-            type:Number
-        },
-        isVisible:{
-            type:Boolean
-        }
-    }
-}
+    deleteEmployee(id) {
+      this.$emit("delete", id);
+    },
+  },
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      default: 15000,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: Number,
+    },
+    isVisible: {
+      type: Boolean,
+    },
+  },
+};
 </script>
 
 <style scoped>
-li{
-    margin: 1rem 0;
-    font-size: 1.25rem;
-    font-weight: bold;
-    background: #8ddba4;
-    border-radius: 25px;
-    color: #1f1f1f;
-    padding: 0.5rem;
+li {
+  margin: 1rem 0;
+  font-size: 1.25rem;
+  font-weight: bold;
+  background: #8ddba4;
+  border-radius: 25px;
+  color: #1f1f1f;
+  padding: 0.5rem;
 }
 
-button{
-    font: inherit;
-    padding: 0.05rem 1rem;
-    border: 1px solid #d83581;
-    background: #d83581;
-    cursor: pointer;
-    color: white;
-    box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
-    margin: 10px;
+button {
+  font: inherit;
+  padding: 0.05rem 1rem;
+  border: 1px solid #d83581;
+  background: #d83581;
+  cursor: pointer;
+  color: white;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+  margin: 10px;
 }
 
+.fade-enter-from{
+    opacity: 0;
+}
+
+.fade-enter-active{
+    transition: all 0.5s linear;
+}
 </style>
